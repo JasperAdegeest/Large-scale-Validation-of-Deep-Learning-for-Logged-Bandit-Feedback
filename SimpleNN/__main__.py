6e06d4d169035c96d5c108eae69c363454a923e6
Jasper Adegeest <jasper.adegeest@gmail.com>
2018-11-26 12:04:31 +0100

    Push voor Vernie

--- SimpleNN/__main__.py
+++ SimpleNN/__main__.py
@@ -9,51 +9,32 @@ from SimpleNN.data import CriteoDataset, BatchIterator
 def run_test_set(model, test_set, batch_size):
     model.eval()
     with torch.no_grad():
-        # THIS IS HOW I THINK IT MUST BE
-        # R = 0
-        # R_N = 0
-        # C = 0
-        # C_N = 0
-        #
-        # for sample, click, propensity in BatchIterator(test_set, batch_size):
-        #     output = model(sample)
-        #     current_batch_size = output.shape[0]
-        #     click = click.view(-1, 1)
-        #
-        #     clicked_tensor = torch.ones(current_batch_size, 1)
-        #     not_clicked_tensor = torch.ones(current_batch_size, 1) * 10
-        #     o = torch.where((click == 0.999), clicked_tensor, not_clicked_tensor)
-        #
-        #     # Calculate R
-        #     R += (click * (output[:, 0, 0] * propensity).view(-1, 1) * o).sum()
-        #     R_N += o.sum()
-        #
-        #     # Calculate C
-        #     C += ((output[:, 0, 0] * propensity).view(-1, 1) * o).sum()
-        #     C_N += o.sum()
-        #
-        # R = (R / R_N) * 10**4
-        # C = C / C_N
-        # R_div_C = R / C
-
-        # THIS GETS AN APPROPRIATE R
-        R = []
-        C = []
+        R = 0
+        C = 0
+        N = 0
 
         for sample, click, propensity in BatchIterator(test_set, batch_size):
             output = model(sample)
+            num_candidates = output.shape[1]
+            current_batch_size = output.shape[0]
             click = click.view(-1, 1)
+            rectified_label = torch.where((click == 0.999),
+                                torch.zeros(current_batch_size, 1),
+                                torch.ones(current_batch_size, 1))
 
-            weight = (output[:, 0, 0] * propensity).view(-1, 1)
+            clicked_tensor = torch.ones(current_batch_size, 1)
+            not_clicked_tensor = torch.ones(current_batch_size, 1) * 10
+            o = torch.where((click == 0.999), not_clicked_tensor, clicked_tensor)
+            N += o.sum()
 
             # Calculate R
-            R.extend(click * weight)
+            R += (rectified_label * ((1/num_candidates) / propensity).view(-1, 1)).sum()
 
             # Calculate C
-            C.extend(weight)
+            C += (((1/num_candidates) / propensity).view(-1, 1)).sum()
 
-        R = np.average(R) * 10 ** 4
-        C = np.average(C)
+        R = (R / N) * 10**4
+        C = C / N
         R_div_C = R / C
 
         print("\nTest results:")
@@ -67,7 +48,7 @@ if __name__ == "__main__":
     parser.add_argument('--lamb', type=float, default=0.5)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--stop_idx', type=int, default=5000)
+    parser.add_argument('--stop_idx', type=int, default=1000000)
     parser.add_argument('--embedding_dim', type=int, default=20)
     parser.add_argument('--hidden_dim', type=int, default=100)
     parser.add_argument('--feature_dict', type=str, default='../data/features_to_keys.json')
@@ -83,7 +64,6 @@ if __name__ == "__main__":
 
         losses = []
         run_test_set(model, test_set, args.batch_size)
-
 
         for sample, click, propensity in BatchIterator(train_set, args.batch_size):
             optimizer.zero_grad()
