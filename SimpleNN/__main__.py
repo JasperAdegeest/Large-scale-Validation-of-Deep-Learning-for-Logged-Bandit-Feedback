01c682cefe9219576e3265fe1335a590cef3c1ff
Verna Dankers <verna_dankers@hotmail.com>
2018-11-26 12:34:56 +0100

    For Jasper

--- SimpleNN/__main__.py
+++ SimpleNN/__main__.py
@@ -17,13 +17,13 @@ def run_test_set(model, test_set, batch_size):
             output = model(sample)
             current_batch_size = output.shape[0]
             click = click.view(-1, 1)
-            rectified_label = torch.where((click == 0.999),
+            rectified_label = torch.where((click == 0),
                                 torch.zeros(current_batch_size, 1),
                                 torch.ones(current_batch_size, 1))
 
             clicked_tensor = torch.ones(current_batch_size, 1)
             not_clicked_tensor = torch.ones(current_batch_size, 1) * 10
-            o = torch.where((click == 0.999), not_clicked_tensor, clicked_tensor)
+            o = torch.where((click == 0), not_clicked_tensor, clicked_tensor)
             N += o.sum()
 
             # Calculate R
@@ -42,15 +42,15 @@ def run_test_set(model, test_set, batch_size):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--train', default='../data/vw_compressed_train')
-    parser.add_argument('--test', default='../data/vw_compressed_test')
+    parser.add_argument('--train', default='data/vw_compressed_train')
+    parser.add_argument('--test', default='data/vw_compressed_test')
     parser.add_argument('--lamb', type=float, default=0.5)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=256)
     parser.add_argument('--stop_idx', type=int, default=50000)
     parser.add_argument('--embedding_dim', type=int, default=20)
     parser.add_argument('--hidden_dim', type=int, default=100)
-    parser.add_argument('--feature_dict', type=str, default='../data/features_to_keys.json')
+    parser.add_argument('--feature_dict', type=str, default='data/feature_to_keys.json')
     args = parser.parse_args()
     train_set = CriteoDataset(args.train, args.feature_dict, args.stop_idx)
     test_set = CriteoDataset(args.test, args.feature_dict, args.stop_idx)
@@ -69,7 +69,7 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             output = model(sample)
             loss = (click - args.lamb) * (output[:, 0, 0] / propensity)
-            loss = -torch.sum(loss)
+            loss = torch.sum(loss)
             losses.append(loss.item())
             loss.backward()
             optimizer.step()
