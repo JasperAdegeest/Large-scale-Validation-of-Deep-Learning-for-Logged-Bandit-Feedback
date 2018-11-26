163bcc32059ac70c0ebfa3770ca370c6428753ec
Jasper Adegeest <jasper.adegeest@gmail.com>
2018-11-26 12:17:41 +0100

    Fixed test function

--- SimpleNN/__main__.py
+++ SimpleNN/__main__.py
@@ -15,7 +15,6 @@ def run_test_set(model, test_set, batch_size):
 
         for sample, click, propensity in BatchIterator(test_set, batch_size):
             output = model(sample)
-            num_candidates = output.shape[1]
             current_batch_size = output.shape[0]
             click = click.view(-1, 1)
             rectified_label = torch.where((click == 0.999),
@@ -28,10 +27,10 @@ def run_test_set(model, test_set, batch_size):
             N += o.sum()
 
             # Calculate R
-            R += (rectified_label * ((1/num_candidates) / propensity).view(-1, 1)).sum()
+            R += (rectified_label * (output[:, 0, 0] / propensity).view(-1, 1)).sum()
 
             # Calculate C
-            C += (((1/num_candidates) / propensity).view(-1, 1)).sum()
+            C += ((output[:, 0, 0] / propensity).view(-1, 1)).sum()
 
         R = (R / N) * 10**4
         C = C / N
@@ -48,7 +47,7 @@ if __name__ == "__main__":
     parser.add_argument('--lamb', type=float, default=0.5)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch_size', type=int, default=256)
-    parser.add_argument('--stop_idx', type=int, default=1000000)
+    parser.add_argument('--stop_idx', type=int, default=50000)
     parser.add_argument('--embedding_dim', type=int, default=20)
     parser.add_argument('--hidden_dim', type=int, default=100)
     parser.add_argument('--feature_dict', type=str, default='../data/features_to_keys.json')
@@ -59,17 +58,18 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters())
 
     epoch_losses = []
+    print("Initialized dataset")
+    run_test_set(model, test_set, args.batch_size)
+
     for i in range(args.epochs):
         print("Starting epoch {}".format(i))
 
         losses = []
-        run_test_set(model, test_set, args.batch_size)
-
         for sample, click, propensity in BatchIterator(train_set, args.batch_size):
             optimizer.zero_grad()
             output = model(sample)
             loss = (click - args.lamb) * (output[:, 0, 0] / propensity)
-            loss = torch.sum(loss)
+            loss = -torch.sum(loss)
             losses.append(loss.item())
             loss.backward()
             optimizer.step()
