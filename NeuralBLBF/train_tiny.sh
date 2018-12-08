f1e064f65e2e6d0c4934246a45daf7f0ab93162d
Michiel van der Meer <michiel@dutchnaoteam.nl>
2018-12-08 18:18:26 +0100

    Find NeuralBLBF module

--- NeuralBLBF/train_tiny.sh
+++ NeuralBLBF/train_tiny.sh
@@ -17,6 +17,7 @@ module load eb
 module load Python/3.6.3-foss-2017b
 module load cuDNN/7.0.5-CUDA-9.0.176
 module load NCCL/2.0.5-CUDA-9.0.176
+export PYTHONPATH=$PYTHONPATH:$HOME/IR2
 
 DATA_TRAIN="$HOME/IR2/data/vw_compressed_train"
 DATA_TEST="$HOME/IR2/data/vw_compressed_validate"
