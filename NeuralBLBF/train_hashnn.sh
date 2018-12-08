35dafafa07bfb28f1f0c9d74f46989fe473fe843
Michiel van der Meer <michiel@dutchnaoteam.nl>
2018-12-08 17:15:47 +0100

    Added train scripts for LISA

---
+++ NeuralBLBF/train_hashnn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Specification of the job requirements for the batch system (number of nodes, expected runtime, etc)
+#SBATCH --job-name=small
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=4:00:00
+#SBATCH --mem=60000M
+#SBATCH --partition=gpu_shared
+#SBATCH --gres=gpu:1
+
+
+module purge
+module load eb
+
+module load Python/3.6.3-foss-2017b
+module load cuDNN/7.0.5-CUDA-9.0.176
+module load NCCL/2.0.5-CUDA-9.0.176
+
+DATA_TRAIN="data/train"
+DATA_TEST="data/validate"
+EPOCHS=10
+LAMBDA=1
+EMBD_DIM=256
+MODEL_TYPE="HashFFNN2"
+
+python3 -m NeuralBLBF --device_id 0 --train $DATA_TRAIN --test $DATA_TEST --epochs $EPOCHS --stop_idx 100000000 --step_size 20000000 --batch_size 128 --enable_cuda --embedding_dim $EMBD_DIM --model LargeEmbedFFNN --save --lamb $LAMBDA
