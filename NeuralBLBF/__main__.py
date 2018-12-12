22327338d594a633c7c4dd7babadd3336e80a803
Michiel van der Meer <michiel@dutchnaoteam.nl>
2018-12-12 11:14:57 +0100

    Moved loading of checkpoint so it actually works

--- NeuralBLBF/__main__.py
+++ NeuralBLBF/__main__.py
@@ -73,15 +73,16 @@ if __name__ == "__main__":
         raise NotImplementedError()
 
     if args["enable_cuda"] and torch.cuda.is_available(): model.to(device)
-    n_params = sum([np.prod(par.size()) for par in model.parameters() if par.requires_grad])
-    optimizer = torch.optim.SGD(model.parameters(), lr=args["learning_rate"], weight_decay=args['weight_decay'])
-    start_epoch = 0
+
     if args['resume'] is not None:
         checkpoint = torch.load(args['resume'])
         model.load_state_dict(checkpoint['model'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         start_epoch = checkpoint['epoch']
 
+    n_params = sum([np.prod(par.size()) for par in model.parameters() if par.requires_grad])
+    optimizer = torch.optim.SGD(model.parameters(), lr=args["learning_rate"], weight_decay=args['weight_decay'])
+    start_epoch = 0
     logging.info("Initialized model and optimizer. Number of parameters: {}".format(n_params))
 
     train(model, optimizer, feature_dict, start_epoch, device, **args)
