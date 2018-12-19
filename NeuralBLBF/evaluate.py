76e55d1a5e754d94c23127c1f48f71514a054147
Jasper Adegeest <jasper.adegeest@gmail.com>
2018-12-19 16:00:59 +0100

    Added standard deviation to evaluate

--- NeuralBLBF/evaluate.py
+++ NeuralBLBF/evaluate.py
@@ -40,12 +40,30 @@ def run_test_set(model, test_filename, batch_size, enable_cuda, sparse,
                 sampling = torch.multinomial(output, 1, replacement=False).cpu().numpy()
                 for c, index, prop in zip(click.cpu().numpy(), sampling, output.cpu().numpy()):
                     f2.write("{}\n".format(prop[index[0]]))
-                
 
-        modified_denom = sum(N_list)
+
+        modifiedDenom = sum(N_list)
         power = 10**4
-        R = (sum(R_list) / modified_denom) * power
-        C = sum(C_list) / sum(N_list)
+        numerator = R_list
+        denominator = C_list
+        maxInstances = len(R_list)
+        scaleFactor = np.sqrt(maxInstances) / modifiedDenom
+
+        R = (np.sum(numerator) / modifiedDenom) * power
+        R_std = 2.58 * np.std(numerator) * scaleFactor  # 99% CI
+        C = np.sum(denominator) / modifiedDenom
+        C_std = 2.58 * np.std(denominator) * scaleFactor  # 99% CI
         R_div_C = R / C
 
-        logging.info("Test Results: R x 10^4: {:.4f}\t C: {:.4f}\t (R x 10^4) / C: {:.4f}".format(R, C, R_div_C))
+        normalizer = C * modifiedDenom
+
+        # See Art Owen, Monte Carlo, Chapter 9, Section 9.2, Page 9
+        # Delta Method to compute an approximate CI for SN-IPS
+        Var = np.sum(np.square(numerator) + \
+                        np.square(denominator) * R_div_C * R_div_C - \
+                        2 * R_div_C * np.multiply(numerator, denominator), dtype=np.longdouble) / (
+                          normalizer * normalizer)
+        R_div_C_std = 2.58 * np.sqrt(Var) / np.sqrt(maxInstances)  # 99% CI
+
+        logging.info("Test Results: R x 10^4: {:.4f}+/-{:.3f}\t C: {:.4f}+/-{:.3f}\t (R x 10^4) / C: {:.4f}+/-{:.3f}"
+                     .format(R, R_std, C, C_std, R_div_C, R_div_C_std))
